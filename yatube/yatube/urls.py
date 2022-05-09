7d951428c0d95681f461882035e6191168b09f90
Кира Изгагина <kira-arti@yandex.ru>
2022-05-09 17:44:27 +0300

    Added migrations, static, templates

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
-    path('', include('posts.urls')),
-    path('group/<slug:slug>/', include('posts.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('posts.urls', namespace='posts')),
+    path('admin/', admin.site.urls)
 ]
